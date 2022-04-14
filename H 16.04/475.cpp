b3c97a1e6c4402feaa561b9f5e0a3dba290fed63
Владислав Зубков КНМО-101 <usualvlad@gmail.com>
2022-04-15 02:02:02 +0500

    Create 475.cpp

---
+++ H 16.04/475.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    ifstream input("input.txt");
+    ofstream out("output.txt");
+    vector <int> vec;
+    while(!input.eof())
+    {
+        int a;
+        input >> a;
+        vec.push_back(a);
+    }
+    if (vec.size()==2)
+        out << "Yes";
+    else {
+        sort(vec.begin(), vec.end());
+        int d = vec[1] - vec[0];
+        for(int i=1; i<vec.size()-1; ++i)
+        {
+            if (vec[i + 1] - vec[i] != d) {
+                out << "No";
+                return 0;
+            }
+        }
+        out << "Yes";
+    }
+    return 0;
+}
+
