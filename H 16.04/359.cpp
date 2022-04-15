014d8b5c06a6fa8c3edf79a7625727f468e796a6
Владислав Зубков КНМО-101 <usualvlad@gmail.com>
2022-04-15 21:54:30 +0500

    Create 359.cpp

---
+++ H 16.04/359.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main() {
+    ifstream input("input.txt");
+    ofstream out("output.txt");
+    int n;
+    input >> n;  
+    int x = (n * n + 1) / 2;
+
+    out << (x - 1) / 9 * 10 + (x - 1) % 9 + 1; 
+    return 0;
+}
