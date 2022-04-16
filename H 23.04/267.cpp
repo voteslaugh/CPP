4a3efea0b1d5452af6ddfc0f0dc436bdc070d96d
Владислав Зубков КНМО-101 <usualvlad@gmail.com>
2022-04-17 01:15:56 +0500

    Create 267.cpp

---
+++ H 23.04/267.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+
+int main()
+{
+	ifstream in("input.txt");
+	ofstream out("output.txt");
+	int n, m;
+	float lists;
+	in >> lists >> n >> m;
+	int i=0;
+	while (lists>=0)
+	{
+		i++;
+		lists -= ((float)1 / n + (float)1 / m);
+	}
+
+	out << i;
+
+    return 0;
+}
