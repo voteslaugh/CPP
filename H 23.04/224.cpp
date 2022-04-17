e1be14e1618a5c7c48ba81bb05e07ed6ad692fe7
Владислав Зубков КНМО-101 <usualvlad@gmail.com>
2022-04-17 15:30:06 +0500

    Create 224.cpp

---
+++ H 23.04/224.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+    long long n;
+    in >> n;
+    vector <short int> vec(n);
+    for (int i = 0; i < n; ++i)
+        in >> vec[i];
+    sort(vec.begin(), vec.end());
+    int mul = 1;
+    int i = 0;
+    for (int j = n-1; j >= 0; --j) {
+        if (vec[j]!=0)
+        {
+            ++i;
+           mul *= vec[j];
+        }
+        if (i == 3)break;
+    }
+
+    out << mul;
+}
