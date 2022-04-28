7b68060491eb77210977afa75e0894f6005bbe48
Владислав Зубков КНМО-101 <usualvlad@gmail.com>
2022-04-28 19:44:30 +0500

    Create 20.cpp

---
+++ H 30.04/20.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+    int n;
+    in >> n;
+    vector <int> vec(n);
+    for (int i = 0; i < n; ++i)
+        in >> vec[i];
+    int cur = vec[0];
+    int prev = vec[0];
+    int prevprev = vec[0];
+    int len = 0, maxlen = 0;
+    for(int i=1; i<n; ++i)
+    {
+        prevprev = prev;
+        prev = cur;
+        cur = vec[i];
+        if ((prev > cur && prev > prevprev) || (prev < cur && prev < prevprev)) len++;
+        else if (prev != cur) len = 2;
+        else len = 1;
+        maxlen = max(maxlen, len);
+    }
+    out << maxlen;
+    return 0;
+}
+
