753e1ea7912aaf52883c26a2abf036257f9d8de3
Владислав Зубков КНМО-101 <usualvlad@gmail.com>
2022-04-28 21:08:04 +0500

    Create 74.cpp

---
+++ H 30.04/74.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+    int n, m;
+    in >> n >> m;
+    if (m==1)
+    {
+        out << n;
+        return 0;
+    }
+    int s = 0;
+    while (m % 2 != 0)
+    {
+        s += n / 2;
+        n -= n / 2;
+        m = (m + 1) / 2;
+    }
+    s += m / 2;
+    out << s;
+    return 0;
+}
+
