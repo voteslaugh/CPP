ad3b9cc4d15dfdcfffb6d44445d9f371ae9d2f33
Владислав Зубков КНМО-101 <usualvlad@gmail.com>
2022-04-28 20:25:13 +0500

    Create 202.cpp

---
+++ H 30.04/202.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+    string str, samp;
+    in >> str >> samp;
+    int i = -1;
+    while (samp.size()<=str.size())
+    {
+        ++i;
+        if (str.substr(0, samp.size()) == samp)
+            out << i << " ";
+        str.erase(0, 1);
+    }
+    return 0;
+}
+
