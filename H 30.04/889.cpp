09e1904a0baeceeb600f0243c1cd23a10df59e7f
Владислав Зубков КНМО-101 <usualvlad@gmail.com>
2022-04-28 19:44:27 +0500

    Create 889.cpp

---
+++ H 30.04/889.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct Hor
+{
+    int x;
+    int y;
+};
+
+int main()
+{
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+    int start, n;
+    in >> start >> n;
+    vector <Hor> h(n);
+    for (int i = 0; i < n; ++i)
+        in >> h[i].x >> h[i].y;
+    sort(h.begin(), h.end(), [](const Hor& left, const Hor& right) 
+        {
+            return left.y > right.y;
+        });
+    int cur = start;
+    for (Hor hor: h)
+    {
+        if (hor.x == cur)
+            cur++;
+        else if (hor.x + 1 == cur)
+            cur--;
+    }
+    out << cur;
+    return 0;
+}
+
