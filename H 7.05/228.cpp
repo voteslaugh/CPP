ebf18e1a8983a1f0952b925e19e17871a644f71e
Владислав Зубков КНМО-101 <usualvlad@gmail.com>
2022-05-04 01:28:52 +0500

    Create 228.cpp

---
+++ H 7.05/228.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+    short int n;
+    in >> n;
+    float dol, eu, prevdol, preveu, mon;
+    mon = 100;
+    in >> prevdol >> preveu;
+    for(int i=0; i<n-1; ++i)
+    {
+        in >> dol >> eu;
+        if(prevdol < dol || preveu < eu)
+        {
+            mon = max(((mon - prevdol*(mon / prevdol)) + (mon / prevdol)*(dol)), ((mon - preveu*(mon / preveu)) + (mon / preveu) * (eu)));
+        }
+        prevdol = dol;
+        preveu = eu;
+    }
+    out << std::fixed << std::setprecision(2) << mon << std::endl;
+    return 0;
+}
