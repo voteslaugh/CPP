f506614eaa7a91bc14401af9ee39710d908f8b93
Владислав Зубков КНМО-101 <usualvlad@gmail.com>
2022-05-04 03:23:15 +0500

    Create 281.cpp

---
+++ H 7.05/281.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+    unsigned short int n, m;
+    unsigned int answ;
+    in >> n >> m;
+
+    if (m == 0)
+        out << n * tgamma(n + 1);
+    else
+        out << tgamma(n + 1) / (tgamma(m + 1) * tgamma(n - m)) + tgamma(n + 1) / (tgamma(n + 1) * tgamma(n - m));
+    return 0;
+}
