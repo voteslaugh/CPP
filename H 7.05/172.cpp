e64611e5c672450fd477ef812c45768b012232f6
Владислав Зубков КНМО-101 <usualvlad@gmail.com>
2022-05-04 01:28:49 +0500

    Create 172.cpp

---
+++ H 7.05/172.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+    string N;
+    long K;
+    in >> N >> K;
+    int i = 0;
+    long answ = 0;
+    while (i < N.length()) {
+        while (answ < K)
+            answ = answ * 10 + N[i++] - '0';
+        answ = answ % K;
+    }
+    out << answ;
+    return 0;
+}
