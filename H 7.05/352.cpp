d7090cad24a4064e196ad38328996959c38f2a9a
Владислав Зубков КНМО-101 <usualvlad@gmail.com>
2022-05-04 03:23:17 +0500

    Create 352.cpp

---
+++ H 7.05/352.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+bool isCoprime(long a, long b) {
+    for (long gcd = a; ; gcd = b, b = a % b, a = gcd)
+        if (!b) return gcd == 1;
+}
+
+int main()
+{
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+    long n, k=2, numer=1, denom;
+    in >> n;
+    denom = n - 1;
+    n-=2;
+
+    while(k<n)
+    {   
+        if(isCoprime(k, n))
+        {
+            numer = k;
+            denom = n;
+        }
+        k++;
+        n--;
+    }
+    out << numer << " " << denom;
+    return 0;
+}
