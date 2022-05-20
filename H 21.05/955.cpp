b3e776cbc16b79d923fba3e1038b19c4b5ac79d3
Владислав Зубков КНМО-101 <usualvlad@gmail.com>
2022-05-20 23:04:56 +0500

    Create 955.cpp

---
+++ H 21.05/955.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include<fstream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+	ifstream in("INPUT.TXT");
+	ofstream out("OUTPUT.TXT");
+	int n, m, k;
+	in >> n >> m >> k;
+	int i, j, l, i1, j1;
+	vector<vector<int>>vec(n, vector<int>(m, 0));
+	for (int l = 0; l < k; ++l) {
+		in >> i >> j;
+		vec[i][j] = -1;
+	}
+	int coun = 1;
+	for (int i = 1; i < n; ++i) {
+		for (int j = 1; k < m; ++k) {
+			if ((vec[i-1][j]==coun || vec[i][j-1]==coun || vec[i+1][j]==coun|| vec[i][j+1]==coun) && vec[i][j]==0) {
+				vec[i][j] = coun;
+			}
+			else if (vec[i][j] == 0) {
+				vec[i][j] = ++coun;
+
+			}
+		}
+	}
+	out << coun << endl;
+	
+}
