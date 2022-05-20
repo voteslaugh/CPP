a2f6062398ab94a31d5daf8d5975fa992fa540a0
Владислав Зубков КНМО-101 <usualvlad@gmail.com>
2022-05-20 09:06:43 +0500

    Create 183.cpp

---
+++ H 21.05/183.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+int getEnts(const size_t &k, const size_t &p) {
+    if (k < 2) {
+        return 0;
+    }
+    std::vector<size_t> num(k + 1);
+    num[2] = 1;
+    for (int i = 2; i < k; i++) {
+        num[i + 1] = (num[i + 1] + num[i]) % p;
+        if (2 * i <= k) {
+            num[2 * i] = num[i];
+        }
+    }
+    return num[k];
+}
+
+int main() {
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+    size_t k, p;
+    in >> k >> p;
+    out<<getEnts(k, p);
+    return 0;
+}
