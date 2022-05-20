ca877f2a22ca750d95d578a243dcfca585a1a87e
Владислав Зубков КНМО-101 <usualvlad@gmail.com>
2022-05-20 23:11:35 +0500

    Update 704.cpp

--- H 21.05/704.cpp
+++ H 21.05/704.cpp
@@ -2,40 +2,48 @@
 #include <vector>
 using namespace std;
 
-float mlt(float x0, float y0, float x1, float y1) 
+float mlt(float x0, float y0, float x1, float y1)
 {
     return x0 * y1 - x1 * y0;
 }
-float scal(float x0, float y0,float x1, float y1)
+
+float scal(float x0, float y0, float x1, float y1)
 {
     return x0 * x1 + y0 * y1;
 }
 
-int sign(float &a)
+int sign(float& a)
 {
     return (a > 0) - (a < 0);
 }
 
-void main()
+int main()
 {
     ifstream in("input.txt");
     ofstream out("output.txt");
+
     int sz, n;
     in >> sz >> n;
+
     vector<int> vec(n * 2);
-    for (int i = 0; i < n*2-1; ++i) {
+    for (int i = 0; i < n * 2 - 1; ++i) {
         in >> vec[i];
     }
+
     bool flag = false;
+
     float x0 = sz / 2;
     float y0 = sz / 2;
-    for(int i=0; i<n; ++i)
+
+    for (int i = 0; i < n; ++i)
     {
         float x1 = vec[2 * i];
         float y1 = vec[2 * i + 1];
+
         int c[3] = { 0,0,0 };
         int c0[3] = { 0,0,0 };
-        for(int j=0; j<n; ++j)
+
+        for (int j = 0; j < n; ++j)
         {
             float x2 = vec[2 * j], y2 = vec[2 * j + 1];
             float vect = mlt(x1 - x0, y1 - y0, x2 - x0, y2 - y0);
@@ -50,4 +58,5 @@ void main()
             flag = true;
     }
     flag ? out << "YES" : out << "NO";
+    return 0;
 }
