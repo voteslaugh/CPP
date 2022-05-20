0f1f364c78fe68615b309ceb73169d5b15b6d1fd
Владислав Зубков КНМО-101 <usualvlad@gmail.com>
2022-05-20 23:01:53 +0500

    Create 42.cpp

---
+++ H 21.05/42.cpp
@@ -0,0 +1,16 @@
+﻿#include <fstream>
+#include <cmath>
+using namespace std;
+int main() {
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+    int n;
+    in >> n;
+    long long maxPow = pow(3, n / 3);
+    if (n % 3 == 1)
+        maxPow = maxPow * 4 / 3;
+    if (n % 3 == 2)
+        maxPow = maxPow * 2;
+ 
+    out << maxPow;
+}
