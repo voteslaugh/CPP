ce5e8eef4b746e22e497701add406599afad9282
Владислав Зубков КНМО-101 <usualvlad@gmail.com>
2022-05-25 12:04:57 +0500

    Create 954.cpp

---
+++ H 28.05/954.cpp
@@ -0,0 +1,26 @@
+﻿#include <fstream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+    int height;
+    in >> height;
+    vector<int>count(height + 1, 0);
+    count[0] = 1;
+    const int MOD = 1000 * 1000;
+    for(int h=1; h<= height; ++h)
+    {
+        if (h >= 10)
+            count[h] += count[h - 10];
+        if (h >= 11)
+            count[h] += count[h - 11];
+        if (h >= 12)
+            count[h] += count[h - 12];
+        count[h] %= MOD;
+    }
+    out << count[height] * 2 % MOD;
+    return 0;
+}
