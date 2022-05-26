3fdcdebd7aa3a92b8f9967a206d29b259b6d20ff
Владислав Зубков КНМО-101 <usualvlad@gmail.com>
2022-05-26 13:32:46 +0500

    Create Seminar2.cpp

---
+++ H 28.05/Seminar2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+template<typename T>
+T sum_all(const vector <T> &vec)
+{
+    T sum = vec[0];
+    for (int i = 1; i < vec.size(); ++i)
+        sum += vec[i];
+    return sum;
+}
+string sum_all(const vector<char> &vec)
+{
+    string sum ="";
+    for (int i = 0; i < vec.size(); ++i)
+        sum += vec[i];
+    return sum;
+}
+template <typename T>
+T sum_all(T a)
+{
+    return a;
+}
+
+int main()
+{
+    vector<int> vec = { 1 , 2, 3 };
+    vector <float> vec1 = { 1, 2, 3 };
+    vector <string> vec2 = { "ab", "cd" };
+    vector<char> vec3 = { 'a', 'b', 'c' };
+    cout << sum_all(vec) << endl << sum_all(vec1) << endl << sum_all(vec2) << endl << sum_all(vec3) << endl<< sum_all(5.5);
+}
