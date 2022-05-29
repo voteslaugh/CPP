266950fb6bfb6fe38c00eaad74f3d86944c9cfd0
Владислав Зубков КНМО-101 <usualvlad@gmail.com>
2022-05-29 22:55:39 +0500

    Create 150.cpp

---
+++ H 4.06/150.cpp
@@ -0,0 +1,35 @@
+﻿#include <fstream>
+#include <vector>
+#include <functional>
+using namespace std;
+
+int main()
+{
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+    int n, s;
+    in >> n >> s;
+    vector <vector<bool>> isFriend(1 + n, vector<bool>(1 + n));
+    for (int i = 1; i <= n; ++i)
+        for (int j = 1; j <= n; ++j)
+        {
+            int v;
+            in >> v;
+            isFriend[i][j] = (v == 1);
+        }
+    vector<bool>visited(1 + n, false);
+    int ans = 0;
+    function<void(int)>visit = [&](int cur)
+    {
+        if (visited[cur])
+            return;
+        visited[cur] = true;
+        ans++;
+        for (int v = 1; v <= n; ++v)
+            if (isFriend[cur][v])
+                visit(v);
+    };
+    visit(s);
+    out << ans - 1;
+    return 0;
+}
