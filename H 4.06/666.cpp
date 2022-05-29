182e3531520727bfdfe25686111e531b0a1d99a7
Владислав Зубков КНМО-101 <usualvlad@gmail.com>
2022-05-29 21:04:40 +0500

    Create 666.cpp

---
+++ H 4.06/666.cpp
@@ -0,0 +1,28 @@
+﻿#include <fstream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+    int pos;
+    in >> pos;
+    for(int step=26; step>=1; --step)
+    {
+        int half = (1 << (step - 1)) - 1;
+        if (pos == 1)
+        {
+            out << (char)('a' + step - 1);
+            return 0;
+        }
+        else if (pos <= 1 + half)
+            pos--;
+        else
+            pos -= 1 + half;
+    }
+    throw 1;
+}
