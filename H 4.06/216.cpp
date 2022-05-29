20d3fe386d76b69e2b4affb761c553fb096672e5
Владислав Зубков КНМО-101 <usualvlad@gmail.com>
2022-05-29 23:47:12 +0500

    Create 216.cpp

---
+++ H 4.06/216.cpp
@@ -0,0 +1,23 @@
+﻿#include <algorithm>
+#include <fstream>
+using namespace std;
+
+int n, m, k, a[150][150];
+int b[150][150];
+
+int main()
+{
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+    int n, sum = 0, max = 0;
+    in >> n;
+    for(int i=0; i<n; ++i)
+    {
+        int value;
+        in >> value;
+        sum += value;
+        max = std::max(max, value);
+    }
+    out << min(sum / 2, sum - max);
+    return 0;
+}
