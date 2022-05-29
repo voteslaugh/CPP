0656fa237563804eb2b9882cd43ea687dfd8680e
Владислав Зубков КНМО-101 <usualvlad@gmail.com>
2022-05-29 22:40:50 +0500

    Create 936.cpp

---
+++ H 4.06/936.cpp
@@ -0,0 +1,51 @@
+﻿#include <fstream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+struct Point
+{
+    int x;
+    int y;
+};
+
+int main()
+{
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+    int mX, mY, n;
+    double r;
+    in >> mX >> mY >> r >> n;
+    vector <Point> p;
+    for(int i=0; i<n; ++i)
+    {
+        int x, y;
+        in >> x >> y;
+        x -= mX;
+        y -= mY;
+        if (sqrt(x * x + y * y) <= r)
+            p.push_back(Point{ x,y });
+    }
+    int ans = 0;
+    for(Point p1: p)
+    {
+        int on = 0;
+        int left = 0;
+        int right = 0;
+        for(Point p2: p)
+        {
+            int cp = p1.x * p2.y - p2.x * p1.y;
+            if (cp < 0)
+                left++;
+            else if (cp > 0)
+                right++;
+            else
+                on++;
+        }
+        ans = max({ ans, on + left, on + right });
+    }
+    out << ans;
+    return 0;
+}
